fa532f98c684ceca50efe46a62fc5ef2832999aa
YOODONGHYUK <ydh8485@gmail.com>
2020-12-06 01:58:38 +0900

    항목 추가, 로컬에 저장,  삭제 기능 구현

--- todo.js
+++ todo.js
@@ -2,19 +2,55 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-    const TODOS_LS = 'toDos'; // LS = localstorage
+    const TODOS_LS = 'toDos';
+    let toDos = [];
 
-    function paintToDo(text) {
-        const li = document.createElement("li"); // html에 요소 추가
-        const delBtn = document.createElement("button");
-        delBtn.innerText = "X";
-        const span = document.createElement("span");
-        span.innerText = text;
-        li.appendChild(delBtn);
-        li.appendChild(span); //span을 father element인 li안에 넣기
-        toDoList.appendChild(li); // 위 과정을 통해 만든 li를 toDoList(ul)에 넣기
+    // delete
+    function deleteToDo(event) {
+        const btn = event.target;
+        const li = btn.parentNode;
+        toDoList.removeChild(li);
+        const cleanToDos = toDos.filter(function(toDo) {
+            
+            return toDo.id !== parseInt(li.id);
+        });
+        toDos = cleanToDos; // toDos배열에서 항목 제거
+        saveToDos();    // local storage에서 항목 제거
     }
 
+    // save to local storage
+    function saveToDos() {
+        localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); 
+    }
+
+    // show ToDo
+    function paintToDo(text) {
+        const li = document.createElement("li");
+        const delBtn = document.createElement("button");
+        const newId = toDos.length + 1;
+        const span = document.createElement("span");
+
+        delBtn.innerText = "X";
+        delBtn.addEventListener("click", deleteToDo);
+        span.innerText = text;
+
+        // li 추가
+        li.appendChild(delBtn);
+        li.appendChild(span);
+        li.id = newId;  // <li id="1">...<li>
+
+        toDoList.appendChild(li);
+
+        const toDoObj = {
+            text: text,
+            id: newId
+        };
+
+        toDos.push(toDoObj);
+        saveToDos();
+    }
+
+    // input, add ToDo
     function handleSubmit(event) {
         event.preventDefault();
         const currentValue = toDoInput.value;
@@ -22,12 +58,16 @@ const toDoForm = document.querySelector(".js-toDoForm"),
         toDoInput.value = "";
     }
 
-
+    // local storage -> web
     function loadToDos() {
-        const toDos = localStorage.getItem(TODOS_LS);
+        const loadedToDos = localStorage.getItem(TODOS_LS);
         
-        if(toDos !== null){
-
+        if(loadedToDos !== null){
+            // save -> string, load -> object
+            const parsedToDos = JSON.parse(loadedToDos);
+            parsedToDos.forEach(function(toDo){ 
+                paintToDo(toDo.text); 
+            });
         }
     }
 
